4eb8a316a1791832eb9f94cbaf73acc034032270
Nattfarinn <nattfarinn@gmail.com>
2021-12-03 14:46:12 +0100

    [ibexa/content] Enable ibexa encore build

--- ibexa/content/4.0.x-dev/encore/webpack.config.js
+++ ibexa/content/4.0.x-dev/encore/webpack.config.js
@@ -1,8 +1,10 @@
 const Encore = require('@symfony/webpack-encore');
 const path = require('path');
 const getEzConfig = require('./ez.webpack.config.js');
+const getIbexaConfig = require('./ibexa.webpack.config.js');
 const eZConfigManager = require('./ez.webpack.config.manager.js');
 const eZConfig = getEzConfig(Encore);
+const ibexaConfig = getIbexaConfig(Encore);
 const customConfigs = require('./ez.webpack.custom.configs.js');
 
 Encore.reset();
@@ -37,7 +39,7 @@ Encore.addEntry('welcome_page', [
 Encore.addEntry('app', './assets/app.js');
 
 const projectConfig = Encore.getWebpackConfig();
-module.exports = [ eZConfig, ...customConfigs, projectConfig ];
+module.exports = [ eZConfig, ibexaConfig, ...customConfigs, projectConfig ];
 
 // uncomment this line if you've commented-out the above lines
 // module.exports = [ eZConfig, ...customConfigs ];
