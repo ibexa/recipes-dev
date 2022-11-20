150302e65c91507068f915ac14eae8df234b4536
Adam Wójs <adam@wojs.pl>
2022-11-20 18:13:59 +0100

    Added bash script to initialize recipes for next Ibexa DXP version base on previous one

---
+++ bin/init-recipies.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Initialize recipes for next Ibexa DXP version base on previous one.
+if [ ! -n "$1" ]
+then
+    echo "Usage: `basename $0` <prev-version> <next-version>"
+    exit 1
+fi
+
+PREV_VERSION=$1;
+NEXT_VERSION=$2;
+
+for package in ibexa/*; do
+  if [[ -d "$package/$PREV_VERSION" ]]
+  then
+    cp -rf $package/$PREV_VERSION/* "$package/$NEXT_VERSION"
+  fi
+done
