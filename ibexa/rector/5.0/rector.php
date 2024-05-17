e13df6989321998d3bfbf12843c8c88ec770d6c1
Andrew Longosz <alongosz@users.noreply.github.com>
2024-05-17 13:49:33 +0200

    IBX-8117: [ibexa/rector] Added default configuration for ibexa/rector (#119)
    For more details see https://issues.ibexa.co/browse/IBX-8117 and https://github.com/ibexa/recipes-dev/pull/119
    
    Key changes:
    
    * Added default configuration for ibexa/rector
    
    * Added manifest.json
    
    ---------
    
    Co-authored-by: Konrad Oboza <konradoboza@users.noreply.github.com>

---
+++ ibexa/rector/5.0/rector.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths(
+        [
+            __DIR__ . '/src', // see if it matches your project structure
+            __DIR__ . '/tests',
+        ]
+    );
+
+    // define set lists
+    $rectorConfig->sets(
+        [
+            __DIR__ . '/vendor/ibexa/rector/src/contracts/Sets/ibexa-50.php', // set list for upgrading to Ibexa DXP 5.0
+        ]
+    );
+};
