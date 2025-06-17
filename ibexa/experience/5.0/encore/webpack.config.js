68522921a83b2751cbddb1fd618f82bfb867f163
Michał Grabowski <michal.grabowski@ibexa.co>
2025-06-17 16:46:41 +0200

    IBX-10173: Extract webpack files from root dir

--- ibexa/experience/5.0/encore/webpack.config.js
+++ ibexa/experience/5.0/encore/webpack.config.js
@@ -1,11 +1,13 @@
-const Encore = require('@symfony/webpack-encore');
 const path = require('path');
-const getIbexaConfig = require('./ibexa.webpack.config.js');
-const ibexaConfig = getIbexaConfig(Encore);
-const customConfigs = require('./ibexa.webpack.custom.configs.js');
-const {
-    isReactBlockPathCreated,
-} = require('./ibexa.webpack.config.react.blocks.js');
+const fs = require('fs');
+
+const Encore = require('@symfony/webpack-encore');
+const enableWebpackSymlinks = require('@ibexa/frontend-config/dev/enable-symlinks');
+const getIbexaConfig = require('@ibexa/frontend-config/webpack-config');
+const customConfigs = require('@ibexa/frontend-config/webpack-config/custom');
+
+const ibexaConfig = getIbexaConfig(Encore, null, enableWebpackSymlinks);
+const isReactBlockPathCreated = fs.existsSync('./assets/page-builder/react/blocks');
 
 Encore.reset();
 Encore.setOutputPath('public/build/')
